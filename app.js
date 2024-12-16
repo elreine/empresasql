e995b2a3c8a084754f1bfd936b9a5084c05ae12c
elreine <reinelgarciamorales228@gmail.com>
2024-12-16 10:42:51 +0000

    Arreglar bbdd Vercel

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ const { Pool } = require('pg');
 require('dotenv').config(); // Cargar variables de entorno desde .env
 const app = express();
 
+// Configuración de la base de datos
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL || process.env.DATABASE_LOCAL,
     ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
 });
 
-
+// Servir archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Servir archivos estáticos desde la carpeta 'public'
+// Ruta principal
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
@@ -22,16 +23,15 @@ app.get('/empleados', async (req, res) => {
     try {
         // Consultar todos los empleados desde la base de datos
         const result = await pool.query('SELECT * FROM empleados');
-        
-        // Devolver los datos como JSON
         res.json(result.rows);
     } catch (err) {
-        console.error(err); // Imprimir errores en la consola
+        console.error('Error en la conexión con la base de datos:', err);
         res.status(500).send('Error en la conexión con la base de datos');
     }
 });
 
 // Iniciar el servidor
-app.listen(3000, () => {
-    console.log('Servidor corriendo en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
